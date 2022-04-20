bb0910937ac16ba84714f23cc6343613bed7888d
jennj <jennjenn93@gmail.com>
2022-04-19 22:58:22 -0500

    linking mysql into server.js

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const PORT = process.env.PORT || 3001;
 
 const app=express();
 
+const mysql = require('mysql2');
+
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'Lauren93Aggie2015',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
