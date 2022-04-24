107c53a0ff2dd40458bfe30aa6b9ade8591f1786
jennj <jennjenn93@gmail.com>
2022-04-24 18:54:43 -0500

    joining tables together

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES
 ('Heroes of HTML', 'pppppppppppppppppppppppppppppppppppp'),
 ('Git Gurus', 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww');
 
-INSERT INTO candidates (first_name, last_name, industry_connected)
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
 VALUES
 ('Ronald', 'Firbank', 1, 1),
 ('Virginia', 'Woolf', 1, 1),
